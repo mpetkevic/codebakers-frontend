a10cd24a74747405dc41b1d230df6364083188c2
Miroslav P <miroslav.petkevic@gmail.com>
2019-03-04 19:57:19 +0200

    Updates

---
+++ precache-manifest.55f117cdbf16d4af298bc4a116cccc9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7541b1032d9932012ca4",
+    "url": "/codebakers-frontend/static/js/runtime~main.7541b103.js"
+  },
+  {
+    "revision": "01acabc10ee9a7d345f9",
+    "url": "/codebakers-frontend/static/js/main.01acabc1.chunk.js"
+  },
+  {
+    "revision": "31290a334283c9109e9f",
+    "url": "/codebakers-frontend/static/js/2.31290a33.chunk.js"
+  },
+  {
+    "revision": "01acabc10ee9a7d345f9",
+    "url": "/codebakers-frontend/static/css/main.cea8ad61.chunk.css"
+  },
+  {
+    "revision": "8f3989626774a3cc3ce4e34c39ee3590",
+    "url": "/codebakers-frontend/index.html"
+  }
+];
